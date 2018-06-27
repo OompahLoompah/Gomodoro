2566c9da457eb65bf184ff9b7e5b888bc01daa9a
Sean Heuer <kalterplanet@yahoo.com>
2018-06-26 21:25:16 -0400

    edit timer func to use updated metrics pkg

--- pkg/timer/timer.go
+++ pkg/timer/timer.go
@@ -1,33 +1,28 @@
 package timer
 
 import (
+	"fmt"
 	"time"
-	json "encoding/json"
 
 	metrics "github.com/OompahLoompah/Gomodoro/pkg/metrics"
 )
 
-type timerMetric struct {
-	Seconds   int
-	Cancelled bool
-}
-
-func Timer(t int, notifier func(), sendMetrics bool) error {
-	time.Sleep(time.Duration(t) * time.Second)
+func Timer(seconds int, notifier func(), sendMetrics bool, userTag string) error {
+	time.Sleep(time.Duration(seconds) * time.Second)
 	if notifier != nil {
 		notifier()
 	}
 
 	if sendMetrics {
-		w := timerMetric{t, false}
-		m, err := json.Marshal(w)
-		if err != nil {
-			return err
-		}
-		err = metrics.Push(m, "")
-		if err != nil {
-			return err
-		}
+		measurement := "pomodoro"
+		tag := make(map[string]string)
+		tag["SessionType"] = userTag
+
+		metric := make(map[string]string)
+		metric["Seconds"] = fmt.Sprintf("%d", seconds)
+		metric["Completed"] = "true" //This is a bad assumption but we're using it for now
+		t := time.Now()
+		metrics.Log(measurement, tag, metric, &t)
 	}
 	return nil
 }
