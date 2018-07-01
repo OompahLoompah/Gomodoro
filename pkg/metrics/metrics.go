8383d05b7a6fe7b280c1132dc72d5b39114f4068
Sean Heuer <kalterplanet@yahoo.com>
2018-06-30 23:32:45 -0400

    Add metrics cache function

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -7,27 +7,33 @@ import (
 	"time"
 )
 
-func push(message []byte, srv string) error{
-	fmt.Println("Message is: " + string(message))
+var path string = "/Users/seanheuer/.gomodoro_cache"
+
+func push(msg []byte, srv string) error{
+	fmt.Println("Message is: " + string(msg))
 	if srv == "" {
 		addr := os.Getenv("GOMO_METRICS_SRV")
 		if addr == "" {
 			//TODO: log error sensibly instead of ignoring or mixing non-fatal and fatal errs in return. 
+			cache(msg)
 			return nil
 		}
 		port := os.Getenv("GOMO_METRICS_PORT")
 		if port == "" {
 			//TODO: log error sensibly instead of ignoring or mixing non-fatal and fatal errs in return. 
+			cache(msg)
 			return nil
 		}
 		srv = addr + ":" + port
 	}
 	conn, err := net.Dial("tcp",srv)
 	if err != nil {
+		cache(msg)
 		return err
 	}
-	_, err = conn.Write(message)
+	_, err = conn.Write(msg)
 	if err != nil {
+		cache(msg)
 		err = conn.Close()
 		if err != nil{
 			return err
@@ -70,3 +76,47 @@ func Log(measurement string, tags map[string]string, fieldSet map[string]string,
 	}
 	return nil
 }
+
+func cache(msg []byte) error {
+
+	_, err := os.Stat(path)
+	if err != nil {
+		f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0700)
+		if err != nil {
+			fmt.Println("1")
+			fmt.Println(err)
+			return err
+		}
+		f.Write(msg)
+	} else {
+		f, err := os.OpenFile(path, os.O_RDWR, 0000) //file perms shouldn't matter here since file exists
+		if err != nil {
+			fmt.Println("2")
+			fmt.Println(err)
+			return err
+		}
+		_, err = f.Seek(0,2)
+		if err != nil {
+			return err
+		}
+		_, err = f.Write([]byte("\n")) //TODO really need to prepend msg with \n but not sure how to properly do it right now
+		if err != nil {
+			return err
+		}
+		_, err = f.Write(msg)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//func flushCache() error {
+	//f, err := os.Open(path)
+	//if err != nil {
+	//	return err
+	//}
+	//msg = f
+
+	//need a read that reads to \n and nothing more... 
+//}
